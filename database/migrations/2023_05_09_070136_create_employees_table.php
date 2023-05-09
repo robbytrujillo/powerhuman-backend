51d88349e6c67e31f9bccbaf100bbb1033909252
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-05-09 14:46:40 +0700

    migration table at powerhuman backend| table employee

--- database/migrations/2023_05_09_070136_create_employees_table.php
+++ database/migrations/2023_05_09_070136_create_employees_table.php
@@ -13,6 +13,20 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('gender')->nullable();
+            $table->integer('age')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('photo')->nullable();
+            
+            $table->bigInteger('team_id')->nullable();
+            $table->bigInteger('role_id')->nullable();
+            
+            $table->boolean('is_verified')->default(false);
+            $table->timestamp('verified_at')->nullable();
+
+            $table->softDeletes();
             $table->timestamps();
         });
     }
