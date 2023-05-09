2a3eff260b9d2f0eef8cf2c2df099d144d6e1262
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-05-09 14:25:49 +0700

    migration table at powerhuman backend| table teams

--- database/migrations/2023_05_09_065908_create_teams_table.php
+++ database/migrations/2023_05_09_065908_create_teams_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('icon');
+            $table->bigInteger('company_id')->unsigned(); // foreign key
+            $table->softDeletes();
             $table->timestamps();
         });
     }
